29050f7a597cdce6d91f8ae993e18e876296e912
Errornimus <tobi.dannenberg@googlemail.com>
2025-03-23 21:34:43 +0100

    updated delivery driver

--- delivery_driver/Assets/Driver.cs
+++ delivery_driver/Assets/Driver.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] private float steerSpeed = 300f;
+    [SerializeField] private float moveSpeed = 20f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
