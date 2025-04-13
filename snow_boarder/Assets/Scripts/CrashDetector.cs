83d728fe8b0b4856abcc1decd7f0482dafa3ec39
Errornimus <tobi.dannenberg@googlemail.com>
2025-04-13 14:31:59 +0200

    updated snow boarder

--- snow_boarder/Assets/Scripts/CrashDetector.cs
+++ snow_boarder/Assets/Scripts/CrashDetector.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float crashDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Obstacle")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", crashDelay);
         }
     }
--- snow_boarder/Assets/Scripts/FinishLine.cs
+++ snow_boarder/Assets/Scripts/FinishLine.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float finishingDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", finishingDelay);
         }
     }
