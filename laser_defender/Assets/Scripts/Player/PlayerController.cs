daaf863e0cc57aef87e67b3bec1145f4406cad6f
Errornimus <tobi.dannenberg@googlemail.com>
2025-07-02 22:00:48 +0200

    updated laser defender

--- laser_defender/Assets/Scripts/Player/PlayerController.cs
+++ laser_defender/Assets/Scripts/Player/PlayerController.cs
@@ -3,21 +3,59 @@ using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    [field: SerializeField] float MovementSpeed { get; set; } = 6.5f;
+    // Movement with Boundaries
+    [Header("Movement")]
+    [SerializeField] float MovementSpeed = 6.5f;
 
+    [Header("Viewport Boundary Padding")]
+    [SerializeField] float _paddingLeft;
+    [SerializeField] float _paddingRight;
+    [SerializeField] float _paddingTop;
+    [SerializeField] float _paddingBottom;
+
+    Vector2 _minViewportBoundary;
+    Vector2 _maxViewportBoundary;
+
+    // Input System
     private Vector2 _rawInput;
 
+    /* 
+        Unity 
+    */
     private void Update()
     {
         MovePlayer();
     }
 
-    private void MovePlayer()
+    private void Start()
     {
-        Vector3 delta = _rawInput * MovementSpeed * Time.deltaTime;
-        transform.position += delta;
+        InitBounds();
     }
 
+    /* 
+        Logic
+    */
+    private void MovePlayer()
+    {
+        Vector2 delta = _rawInput * MovementSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, _minViewportBoundary.x + _paddingLeft, _maxViewportBoundary.x - _paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, _minViewportBoundary.y + _paddingBottom, _maxViewportBoundary.y - _paddingTop);
+
+        transform.position = newPosition;
+    }
+
+    private void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        _minViewportBoundary = mainCamera.ViewportToWorldPoint(new Vector2(0f, 0f));
+        _maxViewportBoundary = mainCamera.ViewportToWorldPoint(new Vector2(1f, 1f));
+    }
+
+    /* 
+        EVENTS
+    */
     private void OnMove(InputValue value)
     {
         _rawInput = value.Get<Vector2>();
